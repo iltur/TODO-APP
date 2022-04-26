08ce3bb5ced98c2971c6500e7754f685033532d7
iltur <chmielecka.joanna@gmail.com>
2022-04-26 11:27:20 +0200

    Bootstrap,ngx,firebase installed, app.html cleaned

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'todo-app-57ed7',
+    appId: '1:38057183538:web:1b3d1092b84e61767f6f2b',
+    storageBucket: 'todo-app-57ed7.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyA5EV6uxsQv3W4hmjP1kQ9LfnD02XmMAbE',
+    authDomain: 'todo-app-57ed7.firebaseapp.com',
+    messagingSenderId: '38057183538',
+  },
   production: true
 };
