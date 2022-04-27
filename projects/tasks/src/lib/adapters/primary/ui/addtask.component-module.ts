268a7b5f1b380c1eed74804c72bd63294be20144
iltur <chmielecka.joanna@gmail.com>
2022-04-27 18:49:56 +0200

    No task to add button, disabled button

--- projects/tasks/src/lib/adapters/primary/ui/addtask.component-module.ts
+++ projects/tasks/src/lib/adapters/primary/ui/addtask.component-module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddtaskComponent } from './addtask.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-@NgModule({ imports: [CommonModule, ReactiveFormsModule],
-  	declarations: [AddtaskComponent],
-  	providers: [],
-  	exports: [AddtaskComponent] })
+@NgModule({
+	imports: [CommonModule, ReactiveFormsModule, RouterModule],
+	declarations: [AddtaskComponent],
+	providers: [],
+	exports: [AddtaskComponent]
+})
 export class AddtaskComponentModule {
 }
